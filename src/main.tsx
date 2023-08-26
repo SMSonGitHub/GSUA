a5114b4c6355de7c1dc5fd4d8ce784efdcefc724
SMSonGitHub <sabienruffin@yahoo.com>
2023-08-26 13:26:12 -0500

    deleted unnecessary import

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
-import Frame from 'react-frame-component'
 
 ReactDOM.render(
   <React.StrictMode>
